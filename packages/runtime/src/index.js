8030cf81da3edf2979d2528af987b37de5f7aceb
Rajesh Pillai <rajesh@algorisys.com>
2023-04-17 16:17:37 +0530

    h() and related utils

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -1,1 +1,11 @@
+import {h} from "./h";
+
 console.log("This will soon be a frontend framework!")
+
+const view = h('form', { class: 'login-form', action: 'login' }, [
+  h('input', { type: 'text', name: 'user' }),
+  h('input', { type: 'password', name: 'pass' }),
+  h('button', { on: { click: () => {} } }, ['Login'])
+])
+
+console.log(view)
